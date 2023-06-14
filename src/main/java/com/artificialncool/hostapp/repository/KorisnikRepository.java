9c0c23c69087f4160b7f9159f7f85e838057de20
matija <matija.matovic99@gmail.com>
2023-06-14 16:31:50 +0200

    feat(api): Added tests for database connectivity

--- src/main/java/com/artificialncool/hostapp/repository/KorisnikRepository.java
+++ src/main/java/com/artificialncool/hostapp/repository/KorisnikRepository.java
@@ -3,14 +3,13 @@ package com.artificialncool.hostapp.repository;
 import com.artificialncool.hostapp.model.Korisnik;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface KorisnikRepository extends MongoRepository<Korisnik, String> {
     Optional<Korisnik> findByUsername(String username);
 
     Optional<Korisnik> findByEmail(String email);
-
-    long deleteByID(Long ID);
 
     long deleteByUsername(String username);
 
