6c790af3ce9a6220f297ff11b4109d15a75d2169
matija <matija.matovic99@gmail.com>
2023-06-24 15:12:08 +0200

    fix(test): using specific properties for testing

--- src/test/java/com/artificialncool/hostapp/HostappApplicationTests.java
+++ src/test/java/com/artificialncool/hostapp/HostappApplicationTests.java
@@ -3,10 +3,11 @@ package com.artificialncool.hostapp;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.name=test")
 class HostappApplicationTests {
 
 	@Test
